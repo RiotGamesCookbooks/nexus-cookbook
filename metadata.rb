93f6e144b586e1201fcc0fd83c3e4730c01204bf
Kyle Allan <kallan@riotgames.com>
2012-06-21 14:14:57 -0700

    adding centos support and depends on nginx

--- metadata.rb
+++ metadata.rb
@@ -5,9 +5,10 @@ description      "Installs/Configures nexus"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.10.0"
 
-%w{ ubuntu }.each do |os|
+%w{ ubuntu centos }.each do |os|
   supports os
 end
 
 depends "ark"
 depends "java"
+depends "nginx"
