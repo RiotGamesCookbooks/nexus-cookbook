1d17d312f88dc758b6614488c939a37a3a7e7a10
Kyle Allan <kallan@riotgames.com>
2012-08-02 10:52:24 -0700

    use symbols for attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,21 +1,21 @@
-default['nexus']['version']                                           = '2.0.5'
-default['nexus']['user']                                              = 'nexus'
-default['nexus']['group']                                             = 'nexus'
-default['nexus']['url']                                               = "http://www.sonatype.org/downloads/nexus-#{nexus['version']}-bundle.tar.gz"
+default[:nexus][:version]                                      = '2.0.5'
+default[:nexus][:user]                                         = 'nexus'
+default[:nexus][:group]                                        = 'nexus'
+default[:nexus][:url]                                          = "http://www.sonatype.org/downloads/nexus-#{nexus['version']}-bundle.tar.gz"
 
-default['nexus']['port']                                              = '8081'
-default['nexus']['host']                                              = '0.0.0.0'
-default['nexus']['path']                                              = '/nexus'
+default[:nexus][:port]                                         = '8081'
+default[:nexus][:host]                                         = '0.0.0.0'
+default[:nexus][:path]                                         = '/nexus'
 
-default['nexus']['name']                                              = 'nexus'
-default['nexus']['home']                                              = "/usr/local/#{default['nexus']['name']}"
+default[:nexus][:name]                                         = 'nexus'
+default[:nexus][:home]                                         = "/usr/local/#{default['nexus']['name']}"
 
-default['nexus']['nginx_proxy']['listen_port']                        = 8443
-default['nexus']['nginx_proxy']['server_name']                        = 'localhost'
+default[:nexus][:nginx_proxy][:listen_port]                    = 8443
+default[:nexus][:nginx_proxy][:server_name]                    = 'localhost'
 
-default['nexus']['plugins']                                           = ['nexus-groovy-console-plugin', 'nexus-maven-bridge-plugin']
+default[:nexus][:plugins]                                      = ['nexus-groovy-console-plugin', 'nexus-maven-bridge-plugin']
 
-default['nginx']['configure_flags']                                   = 'with-http_ssl_module'
+default[:nginx][:configure_flags]                              = 'with-http_ssl_module'
 
-default['nexus']['nginx']['options']['client_max_body_size']          = '200M'
-default['nexus']['nginx']['options']['client_body_buffer_size']       = '512k'
+default[:nexus][:nginx][:options][:client_max_body_size]       = '200M'
+default[:nexus][:nginx][:options][:client_body_buffer_size]    = '512k'
