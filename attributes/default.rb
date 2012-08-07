6f12f2cfc692bbe78fb409a2c3b1b8e5285d3852
Kyle Allan <kallan@riotgames.com>
2012-08-07 16:19:22 -0700

    Update default attributes to newest version of Nexus OSS
    Nexus 2.1.1 upgrades Jetty, so the jetty.xml had to be copied
    and modified. This will most likely NOT be backwards compatible
    with earlier versions of Nexus.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
-default[:nexus][:version]                                      = '2.0.5'
+default[:nexus][:version]                                      = '2.1.1'
 default[:nexus][:user]                                         = 'nexus'
 default[:nexus][:group]                                        = 'nexus'
-default[:nexus][:url]                                          = "http://www.sonatype.org/downloads/nexus-#{nexus['version']}-bundle.tar.gz"
+default[:nexus][:url]                                          = "http://www.sonatype.org/downloads/nexus-#{node[:nexus][:version]}-bundle.tar.gz"
 
 default[:nexus][:port]                                         = '8081'
 default[:nexus][:host]                                         = '0.0.0.0'
