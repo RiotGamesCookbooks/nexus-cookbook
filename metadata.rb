6cd2b72503ddab7985d45e3947bdfa62ff59c72e
Kyle Allan <kallan@riotgames.com>
2012-08-21 15:45:16 -0700

    updating version and maintainers

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
-maintainer       "Opscode, Inc."
-maintainer_email "cookbooks@opscode.com"
+maintainer       "Riot Games"
+maintainer_email "kallan@riotgames.com"
 license          "Apache 2.0"
 description      "Installs/Configures nexus"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.10.1"
+version          "0.11.0"
 
 %w{ ubuntu centos }.each do |os|
   supports os
