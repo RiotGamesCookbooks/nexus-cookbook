a487b67b45d36793471b2ad20e3f53ccf1076f74
Kyle Allan <kallan@riotgames.com>
2012-08-21 17:54:11 -0700

    implementing actions

--- providers/proxy.rb
+++ providers/proxy.rb
@@ -17,3 +17,106 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+def load_current_resource
+  @current_resource = Chef::Resource::NexusProsy.new(new_resource.name)
+end
+
+action :enable do
+  install_nexus_cli
+
+  unless smart_proxy_enabled?
+    enable_smart_proxy
+    new_resource.updated_by_last_action(true)
+  end
+end
+
+action :disable do
+  install_nexus_cli
+
+  if smart_proxy_enabled?
+    disable_smart_proxy
+    new_resource.updated_by_last_action(true)
+  end
+end
+
+action :add_trusted_key do
+
+  unless certificate_exists?
+    nexus.add_trusted_key(new_resource.certificate, new_resource.description, false)
+    new_resource.updated_by_last_action(true)
+  end
+end
+
+action :delete_trusted_key do
+
+  if trusted_key_exists?
+    nexus.delete_trusted_key(new_resource.id)
+    new_resource.updated_by_last_action(true)
+  end
+end
+
+private
+  
+  def install_nexus_cli
+    package "libxml2-devel" do
+      action :install
+    end.run_action(:install)
+
+    package "libxslt-devel" do
+      action :install
+    end.run_action(:install)
+    
+    chef_gem "nexus_cli" do
+      version "0.7.0"
+    end
+  end
+
+  def smart_proxy_enabled?
+    require 'json'
+    json = JSON.parse(nexus.get_smart_proxy_settings)
+    json["data"]["enabled"]
+  end
+
+  def certificate_exists?
+    require 'json'
+    json = JSON.parse(nexus.get_trusted_keys)
+    trusted_keys = json["data"]
+    trusted_keys.each do |trusted_key|
+      if new_resource.certificate == trusted_key["certificate"]["pem"]
+        return true
+      end
+    end
+    false    
+  end
+
+  def trusted_key_exists?
+    require 'json'
+    json = JSON.parse(nexus.get_trusted_keys)
+    trusted_keys = json["data"]
+    trusted_keys.each do |trusted_key|
+      if new_resource.id == trusted_key["id"]
+        return true
+      end
+    end
+    false
+  end
+
+  def enable_smart_proxy
+    nexus.enable_smart_proxy(false, new_resource.host, new_resource.port)
+  end
+
+  def disable_smart_proxy
+    nexus.enable_smart_proxy(true)
+  end
+
+  def nexus_cli_credentials
+    data_bag_item = Chef::EncryptedDataBagItem.load('nexus', 'credentials')
+    credentials = data_bag_item["default_admin"]
+    {"url" => node[:nexus][:cli][:url], "repository" => node[:nexus][:cli][:repository]}.merge credentials
+  end
+
+  def nexus
+    require 'nexus_cli'
+    @nexus ||= NexusCli::Factory.create(nexus_cli_credentials)
+  end
