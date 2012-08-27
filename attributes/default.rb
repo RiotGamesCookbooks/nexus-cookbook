d0bfaa8783f769ddaaed53c13373f0180edb5884
jhowarth <jhowarth@riotgames.com>
2012-08-27 09:33:07 -0700

    Add nexus::cli recipe that installs the nexus_cli gem.

--- attributes/default.rb
+++ attributes/default.rb
@@ -25,5 +25,6 @@ default[:nexus][:nginx][:options][:client_body_buffer_size]    = '512k'
 
 default[:nexus][:cli][:url]                                    = "https://#{node[:nexus][:nginx_proxy][:server_name]}:#{node[:nexus][:nginx_proxy][:listen_port]}/nexus"
 default[:nexus][:cli][:repository]                             = "releases"
+default[:nexus][:cli][:packages]                               = ["libxml2-devel", "libxslt-devel"]
 
 default[:nexus][:create_repositories]                          = ["Artifacts"]
