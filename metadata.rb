3ade206d49870ef51a2a1c6106cdec0c45a8c48a
jhowarth <jhowarth@riotgames.com>
2012-08-27 10:26:00 -0700

    bump 0.11.1

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "kallan@riotgames.com"
 license          "Apache 2.0"
 description      "Installs/Configures nexus"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.11.0"
+version          "0.11.1"
 
 %w{ ubuntu centos }.each do |os|
   supports os
