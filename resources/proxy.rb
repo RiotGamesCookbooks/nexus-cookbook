407e0ca7fd26fb74def5b843c28e0f612e0f80f5
Kyle Allan <kallan@riotgames.com>
2012-08-27 16:03:18 -0700

    fix spacing in proxy resource

--- resources/proxy.rb
+++ resources/proxy.rb
@@ -21,7 +21,7 @@
 actions :enable, :disable, :add_trusted_key, :delete_trusted_key
 
 attribute :name, :kind_of           => String, :name_attribute => true
-attribute :id, :kind_of           => String
+attribute :id, :kind_of             => String
 attribute :host, :kind_of           => String
 attribute :port, :kind_of           => Fixnum
 attribute :certificate, :kind_of    => String
