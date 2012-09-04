1a0a405291ad5585fc9dd98e3c707a33dffc15c4
Kyle Allan <kallan@riotgames.com>
2012-09-04 15:29:02 -0700

    update versions for a working nginx cookbook

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "kallan@riotgames.com"
 license          "Apache 2.0"
 description      "Installs/Configures nexus"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.11.3"
+version          "0.12.0"
 
 %w{ ubuntu centos }.each do |os|
   supports os
@@ -11,5 +11,5 @@ end
 
 depends "ark", "~> 0.0.11"
 depends "java", "~> 1.5.2"
-depends "nginx", "~> 0.101.6"
+depends "nginx", "~> 1.0.0"
 depends "bluepill", "~> 1.0.6"
