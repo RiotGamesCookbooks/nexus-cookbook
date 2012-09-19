65783f2231bfcba10b077b6181b4dca9d164b7f6
Kyle Allan <kallan@riotgames.com>
2012-09-18 18:32:11 -0700

    up version

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "kallan@riotgames.com"
 license          "Apache 2.0"
 description      "Installs/Configures nexus"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.12.3"
+version          "0.12.4"
 
 %w{ ubuntu centos }.each do |os|
   supports os
