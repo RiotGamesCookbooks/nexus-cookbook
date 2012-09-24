ac98a5ad529cf7d7d9d2a6d06b2912a63914fd06
Kyle Allan <kallan@riotgames.com>
2012-09-24 12:40:49 -0700

    add logic for logging lwrp. use newer nexus_cli gem

--- providers/logging.rb
+++ providers/logging.rb
@@ -17,8 +17,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
 def load_current_resource
-  @current_resource = Chef::Resource::NexusLogging.new(new_resource.level)
+  @current_resource = Chef::Resource::NexusLogging.new(new_resource.name)
 
   run_context.include_recipe "nexus::cli"
 
@@ -26,4 +27,18 @@ def load_current_resource
 end
 
 action :set_level do
+  
+  unless same_logging_level?
+
+    Chef::Nexus.nexus(node).set_logger_level(new_resource.level)
+    new_resource.updated_by_last_action(true)
+  end
 end
+
+private
+  
+  def same_logging_level?
+    require 'json'
+    logging_info = JSON.parse(Chef::Nexus.nexus(node).get_logging_info)
+    json["data"]["rootLoggerLevel"] == new_resource.level
+  end
