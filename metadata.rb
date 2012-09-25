008a5625b066007e5ba14868efea1203b7aabcfc
Kyle Allan <kallan@riotgames.com>
2012-09-25 16:10:47 -0700

    up version and use berkshelf 0.5.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "kallan@riotgames.com"
 license          "Apache 2.0"
 description      "Installs/Configures nexus"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.13.0"
+version          "0.14.0"
 
 %w{ ubuntu centos }.each do |os|
   supports os
