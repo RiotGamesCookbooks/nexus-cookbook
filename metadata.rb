ac53ee517b73fd7da37c8232cac53fd557699b2b
Kyle Allan <kallan@riotgames.com>
2013-01-08 11:17:07 -0800

    use thorscmversion appropriately

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "kallan@riotgames.com"
 license          "Apache 2.0"
 description      "Installs/Configures nexus"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.17.0"
+version          IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue "0.0.1"
 
 %w{ ubuntu centos }.each do |os|
   supports os
