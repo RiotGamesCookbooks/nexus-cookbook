04f160648a578fed71ca230cab909a8146590b11
Kyle Allan <kallan@riotgames.com>
2013-01-30 16:48:37 -0800

    add an service enable resource

--- recipes/default.rb
+++ recipes/default.rb
@@ -219,6 +219,10 @@ artifact_deploy node[:nexus][:name] do
 end
 
 service "nexus" do
+  action :enable
+end
+
+service "nexus" do
   action   :start
   provider Chef::Provider::Service::Init
   notifies :restart, "service[nginx]", :immediately
