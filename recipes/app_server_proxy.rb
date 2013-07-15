6110e5c48ea752b20b860700a135c2bbe3fac3cf
Erik Thorselius <erik.thorselius@gmail.com>
2013-07-15 08:26:06 +0200

    Changed attribute key format.

--- recipes/app_server_proxy.rb
+++ recipes/app_server_proxy.rb
@@ -27,7 +27,7 @@ directory "#{node[:nginx][:dir]}/shared/certificates" do
   recursive true
 end
 
-ssl_files = Chef::Nexus.get_ssl_files_data_bag(node) if node[:nexus][:app_server_proxy][:ssl_enabled]
+ssl_files = Chef::Nexus.get_ssl_files_data_bag(node) if node[:nexus][:app_server_proxy][:ssl][:enabled]
 if ssl_files && ssl_files[node[:nexus][:app_server_proxy][:ssl][:key]]
 
   log "Using nexus_ssl_files data bag entry for #{node[:nexus][:app_server_proxy][:ssl][:key]}" do
