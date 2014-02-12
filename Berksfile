6d3caa4a355a4848d0a8c49f54c8c13d6d94ca16
Kyle Allan <kallan@riotgames.com>
2014-02-11 16:02:07 -0800

    bump nginx dependency

--- Berksfile
+++ Berksfile
@@ -2,3 +2,5 @@ site :opscode
 chef_api :config
 
 metadata
+
+cookbook 'yum', '= 2.4.4'
--- metadata.rb
+++ metadata.rb
@@ -4,12 +4,12 @@ maintainer_email "kallan@riotgames.com"
 license          "Apache 2.0"
 description      "Installs/Configures nexus"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "3.0.2"
+version          "3.1.0"
 
 %w{ ubuntu centos }.each do |os|
   supports os
 end
 
 depends "java", "~> 1.15.4"
-depends "nginx", "~> 1.6.0"
+depends "nginx", "= 1.8.0"
 depends "artifact", "~> 1.11.0"
