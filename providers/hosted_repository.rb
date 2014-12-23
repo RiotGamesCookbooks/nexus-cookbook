f50d0100fc7b1df1ba60199b3c8045d2b87918f6
Maxime Brugidou <m.brugidou@criteo.com>
2014-12-23 18:40:03 +0100

    Allow provider configuration in LWRP
    You can now use the attribute `repo_provider` for hosted, proxy and
    group repositories. This will default to `maven2` but you can use
    * `rubygems-hosted`
    * `rubygems-group`
    * `rubygems-proxy`
    * `nuget-proxy`
    * ...etc
    
    Fixes #97

--- providers/hosted_repository.rb
+++ providers/hosted_repository.rb
@@ -31,7 +31,7 @@ end
 
 action :create do
   unless repository_exists?(@current_resource.name)
-    Chef::Nexus.nexus(node).create_repository(new_resource.name, false, nil, nil, new_resource.policy, nil)
+    Chef::Nexus.nexus(node).create_repository(new_resource.name, false, nil, nil, new_resource.policy, new_resource.repo_provider)
     set_publisher if new_resource.publisher
     new_resource.updated_by_last_action(true)
   end
