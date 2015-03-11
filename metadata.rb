e9ead6d84cb7388179b8d5a3d6cc74ca6d0e6fb0
Leo Simons <lsimons@schubergphilis.com>
2015-03-11 10:32:20 +0100

    TK-3183: set up github.com/schubergphilis fork

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "kallan@riotgames.com"
 license          "Apache 2.0"
 description      "Installs/Configures nexus"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "3.3.2"
+version          "3.3.2001"
 
 %w{ ubuntu centos }.each do |os|
   supports os
