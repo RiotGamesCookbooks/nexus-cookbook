a22ce8fd5dbca33578321be761b82104904f47dc
akadoya <aoi.kadoya@rakops.com>
2016-11-29 13:07:29 -0800

    changing nginx dependency to chef_nginx

--- metadata.rb
+++ metadata.rb
@@ -4,12 +4,12 @@ maintainer_email "kallan@riotgames.com"
 license          "Apache 2.0"
 description      "Installs/Configures nexus"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "3.3.1"
+version          "3.3.2"
 
 %w{ ubuntu centos }.each do |os|
   supports os
 end
 
 depends "java", ">= 1.15.4"
-depends "nginx", "= 2.7.6"
+depends "chef_nginx", "~> 4.0.1"
 depends "artifact", ">= 1.11.0"
